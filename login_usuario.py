cde9163b08a05d280d7969ca4f2f45c753a94711
Bruno Palhano <107354811+jbrunopg@users.noreply.github.com>
2023-03-21 21:30:04 -0300

    Update login_usuario.py

--- login_usuario.py
+++ login_usuario.py
@@ -2,32 +2,48 @@
 # coding: utf-8
 
 
-import customtkinter
+# Importação biblioteca
 
-customtkinter.set_appearance_mode("dark")
-customtkinter.set_default_color_theme("dark-blue")
+import customtkinter as ctk
 
-janela = customtkinter.CTk()
+# Definindo as cores
+
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("dark-blue")
+
+# Criação da janela
+
+janela = ctk.CTk()
 janela.geometry("500x300")
 janela.title("Login Usuário")
 
-def clique():
-    print('Fazer Login')
 
-texto = customtkinter.CTkLabel(janela, text='Fazer Login')
+# Criando os campos E-mail e Senha
+
+texto = ctk.CTkLabel(janela, text='Fazer Login')
 texto.pack(padx=10, pady=10)
 
-email = customtkinter.CTkEntry(janela, placeholder_text= "Seu e-mail")
+email = ctk.CTkEntry(janela, placeholder_text= "Seu e-mail")
 email.pack(padx=10, pady=10)
 
-senha = customtkinter.CTkEntry(janela, placeholder_text= "Seu senha", show='*')
+senha = ctk.CTkEntry(janela, placeholder_text= "Seu senha", show='*')
 senha.pack(padx=10, pady=10)
 
-checkbox = customtkinter.CTkCheckBox(janela, text='Esquece minha senha')
+checkbox = ctk.CTkCheckBox(janela, text='Esquece minha senha')
 checkbox.pack(padx=10, pady=10)
 
-botao = customtkinter.CTkButton(janela, text='Login')
+botao = ctk.CTkButton(janela, text='Login')
 botao.pack(padx=10, pady=10)
+
+# Definindo a função que será executada quando o botão de login for clicado
+
+def fazer_login():
+    usuario = email.get()
+    senha = senha.get()
+    if usuario == "usuario" and senha == "senha":
+        print("Login bem sucedido!")
+    else:
+        print("Usuário ou senha incorretos.")
 
 
 janela.mainloop()
